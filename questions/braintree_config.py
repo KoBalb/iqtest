643e6206c497ac1606d679dec63dfdbb64036b22
KoBalb <artem.kovalyk79@gmail.com  artem.kovalyk79@gmail.com>
2025-04-08 18:47:41 +0300

    добавил платёжку

---
+++ questions/braintree_config.py
@@ -0,0 +1,27 @@
+import braintree
+import os
+
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        environment=braintree.Environment.Sandbox,
+        merchant_id=os.getenv('merchant_id'),
+        public_key=os.getenv('public_key'),
+        private_key=os.getenv('private_key')
+    )
+)
+
+class BraintreeConfig:
+    @staticmethod
+    def generate_client_token():
+        return gateway.client_token.generate()
+
+    @staticmethod
+    def create_transaction(nonce, amount):
+        return gateway.transaction.sale({
+            "amount": amount,
+            "payment_method_nonce": nonce,
+            "options": {
+                "submit_for_settlement": True
+            }
+        })
+
